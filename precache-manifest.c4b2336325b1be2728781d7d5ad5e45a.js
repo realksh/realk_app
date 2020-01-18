866d381dc089f7244b918494c7c0baedd8956a30
kanye <>
2020-01-18 12:52:12 +0700

    Updates

---
+++ precache-manifest.c4b2336325b1be2728781d7d5ad5e45a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "112f821799751d8304f5f706ea7ff0a2",
+    "url": "/realk_app/index.html"
+  },
+  {
+    "revision": "19841375293bcf97b7b8",
+    "url": "/realk_app/static/css/main.309ce4a2.chunk.css"
+  },
+  {
+    "revision": "295dbe91633387dd2801",
+    "url": "/realk_app/static/js/2.5e45fdd7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/realk_app/static/js/2.5e45fdd7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "19841375293bcf97b7b8",
+    "url": "/realk_app/static/js/main.453d3b9f.chunk.js"
+  },
+  {
+    "revision": "15c067467b78145ac497",
+    "url": "/realk_app/static/js/runtime-main.1647a0b3.js"
+  }
+]);
